2def61b0a7acb49637cec2fe0080d01446e66acb
Annalia <annaliadestefano@gmail.com>
2020-03-09 16:18:34 +0200

    made header shrink when scroll down

--- msb/js.js
+++ msb/js.js
@@ -1,3 +1,19 @@
 function closeMenu() {
     document.getElementById("menu").style.display = "none";
 };
+
+function openMenu() {
+    document.getElementById("menu").style.display = "unset";
+};
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        document.getElementById("msb-logo").style.height = "100px";
+        document.getElementById("msb-logo").style.width = "200px";
+    } else {
+        document.getElementById("msb-logo").style.height = "115px";
+        document.getElementById("msb-logo").style.width = "285px";
+    }
+}
